6ca7bf81ea4286ce354739a53126a20d1c6d3372
anniehwa <39128959+anniehwa@users.noreply.github.com>
2018-06-10 23:12:43 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -87,7 +87,7 @@ function parseInput(rplyToken, inputStr) {
 	
 	//鴨霸獸指令開始於此
 	if (trigger.match(/班比|斑比/) != null) return exports.funny.randomReply() ;	
-	if (trigger.match(/運氣|運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
+	if (trigger.match(/運氣|機率/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
 	
 
 	
